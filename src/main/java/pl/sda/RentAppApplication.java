4c72c9965666ec74ac7b89e5ab61f727569d8f4d
KKolekx <katarzyna.kolek92@gmail.com>
2020-12-05 14:11:56 +0100

    initial commit

---
+++ src/main/java/pl/sda/RentAppApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.rent_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/rent_app/RentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.rent_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
